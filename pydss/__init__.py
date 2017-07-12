4f3976f37183607dc4537707bc3d79cb5fb96de1
jmmauricio <jmmauricio6@gmail.com>
2017-07-12 14:29:52 +0200

    new models

---
+++ pydss/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 12:54:49 2017
+
+@author: jmmauricio
+"""
+
+import tools
--- pydss/solvers.py
+++ pydss/solvers.py
@@ -7,8 +7,9 @@ Created on Tue May 23 10:01:08 2017
 """
 
 import numpy as np
+import numba
 
-def nr(xi_0,u_0,lam,Lam, max_iter=1000,tol = 1.0e-8):
+def nrsolve(xi_0,u_0,lam,Lam, max_iter=1000,tol = 1.0e-8):
     '''
     Newton-Raphson method
     
@@ -41,7 +42,7 @@ def nr(xi_0,u_0,lam,Lam, max_iter=1000,tol = 1.0e-8):
     return xi_1
 
 
-def ssa(xi_0,u_0,lam,Lam,h_eval,N_x):
+def ssasolve(xi_0,u_0,lam,Lam,h_eval,N_x):
     
     N_t = xi_0.shape[0]
     N_y = N_t - N_x
@@ -70,3 +71,65 @@ def ssa(xi_0,u_0,lam,Lam,h_eval,N_x):
     
     return lambdas, z,freqs,zetas
 
+
+@numba.jit(nopython=True,cache=True)
+def ssa(struct):
+      
+    N_x = struct[0]['N_x']
+    N_y = struct[0]['N_y']
+    x = struct[0]['x']
+    y = struct[0]['y']
+    
+    F_x = struct[0]['F_x']
+    F_y = struct[0]['F_y']
+    G_x = struct[0]['G_x']
+    G_y = struct[0]['G_y']
+    
+ 
+    A =  F_x - F_y @ np.linalg.inv(G_y) @ G_x
+    lambdas, Phi = np.linalg.eig(A)
+    
+    sigmas = lambdas.real
+    omegas = lambdas.imag
+
+    freqs = omegas/(2*np.pi);
+    zetas = sigmas/np.sqrt(sigmas**2+omegas**2)    
+    
+    return lambdas,freqs,zetas
+
+
+@numba.jit(nopython=True,cache=True)
+def nr(sys_update,struct):
+    N_x = struct[0]['N_x']
+    N_y = struct[0]['N_y']
+    N_l = N_x+N_y
+    Lam = np.zeros((N_l,N_l))
+    lam = np.zeros((N_x+N_y,1))
+    xi_0 = np.ones((N_x+N_y,1))
+    xi_0[0:N_x,:]   = struct[0]['x']
+    xi_0[N_x:N_l,:] = struct[0]['y']    
+    max_iter = 100
+    tol = 1.0e-8
+    sys_update(struct,0,0)
+    for it in range(max_iter):
+        lam[0:N_x,:]   = struct[0]['f']
+        lam[N_x:N_l,:] = struct[0]['g']
+        
+        Lam[0:N_x,0:N_x]     = struct[0]['F_x']
+        Lam[0:N_x,N_x:N_l]   = struct[0]['F_y']
+        Lam[N_x:N_l,0:N_x]   = struct[0]['G_x']
+        Lam[N_x:N_l,N_x:N_l] = struct[0]['G_y']
+        
+        Dxi = np.linalg.solve(-Lam,lam)
+        xi_1 = xi_0 + Dxi
+
+        xi_0 = xi_1
+        
+        struct[0]['x'][:] = xi_0[0  :N_x,:]
+        struct[0]['y'][:] = xi_0[N_x:N_l,:]
+        
+        sys_update(struct,0,0)
+        if np.linalg.norm(Dxi,np.infty)<tol: 
+            #print(it)
+            break
+    return xi_0
